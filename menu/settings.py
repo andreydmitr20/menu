4137de37fee28a34ad1b8fe1c8697d6c51d72ff3
andreydmitr21 <andreydmitr20@gmail.com>
2023-04-20 11:25:30 +0200

    test

--- menu/settings.py
+++ menu/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "www/"
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static/"
 
 
