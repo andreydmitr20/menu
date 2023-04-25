f0c87e7856118028e2707e74857fa66bb797645b
andreydmitr21 <andreydmitr20@gmail.com>
2023-04-25 22:22:27 +0200

    test

--- www/js/login.js
+++ www/js/login.js
@@ -1,3 +1,5 @@
+checkAuth(true);
+
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const okButton = document.querySelector("#okButton");
